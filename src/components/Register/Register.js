f5e2528051abde7b43c97aeb99719e73ae9a6765
panduhjohn <johnrstilwell@codeimmersives.com>
2020-06-29 15:54:49 -0400

    update state in register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,8 +2,101 @@ import React, { Component } from 'react';
 
 import './Register.css';
 export default class Register extends Component {
-    
-    
+    // state = {
+    //     canSubmit: true,
+    //     formSetting: {
+    //         name: {
+    //             name: 'name',
+    //             placeholder: 'Enter name',
+    //             value: '',
+    //             error: {
+    //                 message: '',
+    //                 noError: null,
+    //             },
+    //         },
+    //         username: {
+    //             name: 'username',
+    //             placeholder: 'Enter username',
+    //             value: '',
+    //             error: {
+    //                 message: '',
+    //                 noError: null,
+    //             },
+    //         },
+    //         email: {
+    //             name: 'email',
+    //             placeholder: 'Enter email',
+    //             value: '',
+    //             error: {
+    //                 message: '',
+    //                 noError: null,
+    //             },
+    //         },
+    //         password: {
+    //             name: 'password',
+    //             placeholder: 'Enter password',
+    //             value: '',
+    //             error: {
+    //                 message: '',
+    //                 noError: null,
+    //             },
+    //         },
+    //         city: {
+    //             name: 'city',
+    //             placeholder: 'Enter city',
+    //             value: '',
+    //             error: {
+    //                 message: '',
+    //                 noError: null,
+    //             },
+    //         },
+    //         state: {
+    //             name: 'state',
+    //             placeholder: 'Enter state',
+    //             value: '',
+    //             error: {
+    //                 message: '',
+    //                 noError: null,
+    //             },
+    //         },
+    //         zipCode: {
+    //             name: 'zipCode',
+    //             placeholder: 'Enter zip code',
+    //             value: '',
+    //             error: {
+    //                 message: '',
+    //                 noError: null,
+    //             },
+    //         },
+    //     },
+    //     validate: {
+    //         usernameError: {
+    //             noError: null,
+    //             message: '',
+    //         },
+    //         emailError: {
+    //             noError: null,
+    //             message: '',
+    //         },
+    //         passwordError: {
+    //             noError: null,
+    //             message: '',
+    //         },
+    //         cityError: {
+    //             noError: null,
+    //             message: '',
+    //         },
+    //         stateError: {
+    //             noError: null,
+    //             message: '',
+    //         },
+    //         zipCodeError: {
+    //             noError: null,
+    //             message: '',
+    //         },
+    //     },
+    // };
+
     render() {
         return (
             <>
@@ -51,3 +144,8 @@ export default class Register extends Component {
         );
     }
 }
+
+// const mapStateToProps = (state) => ({});
+
+// export default connect(mapStateToProps, { signupAPI })(Signup);
+// export default connect(mapStateToProps, { signupAPI })(Signup);
