0e535f9aa86ae6e0dbb1939f7e2b988290c94d36
panduhjohn <johnrstilwell@codeimmersives.com>
2020-06-29 15:11:36 -0400

    register and login

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
+import './Register.css';
 export default class Register extends Component {
+    
+    
     render() {
         return (
-            <div>
-                <h1>Hey from Register</h1>
-            </div>
-        )
+            <>
+                <div class='login-box'>
+                    <h1>Register</h1>
+                    <div class='textbox'>
+                        <i class='fas fa-user'></i>
+                        <input
+                            type='text'
+                            placeholder='Username'
+                            name='username'
+                        />
+                    </div>
+                    <div class='textbox'>
+                        <i class='fas fa-at'></i>
+                        <input type='text' placeholder='Email' name='email' />
+                    </div>
+                    <div class='textbox'>
+                        <i class='fas fa-lock'></i>
+                        <input
+                            type='password'
+                            placeholder='Password'
+                            name='password'
+                        />
+                    </div>
+                    <div class='textbox'>
+                        <i class='fas fa-city'></i>
+                        <input type='text' placeholder='City' name='city' />
+                    </div>
+                    <div class='textbox'>
+                        <i class='fas fa-flag-usa'></i>
+                        <input type='text' placeholder='State' name='state' />
+                    </div>
+                    <div class='textbox'>
+                        <i class='fas fa-user'></i>
+                        <input
+                            type='text'
+                            placeholder='Zip Code'
+                            name='zipCode'
+                        />
+                    </div>
+                    <input type='button' class='btn' value='Sign in' />
+                </div>
+            </>
+        );
     }
 }
