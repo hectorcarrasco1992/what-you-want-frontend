9e47713d9b5fa25202bc8a45e05a9b4c65f39649
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2020-07-12 14:29:03 -0400

    made chnges to restaurant

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -53,29 +53,29 @@ export class Restaurant extends Component {
     }
     handleDislike=async (name,id)=>{
 
-    };
-    
-
         try {
             //console.log(id);
             //let activityAPIID = this.props.activities.activities[0].apiID;
             //console.log("id:",activityAPIID);
-
+    
             let user = this.props.authUser.user;
             console.log(id);
             console.log(name);
-
+    
             
             await this.props.dislikeActivity(user,id)
             
             
             
             
-
+    
         } catch (error) {
             console.log(error);
         }
     };
+    
+    
+
 
     render() {
         return (
